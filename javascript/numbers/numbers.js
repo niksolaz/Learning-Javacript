00e47493ba563754f7b3bca6991bae5359690678
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-10 12:43:29 +0000

    Arithmetic operation: subtraction

--- javascript/numbers/numbers.js
+++ javascript/numbers/numbers.js
@@ -12,11 +12,22 @@ var doubleNumber = 20.1233;
 console.log('This is a double number',doubleNumber);
 
 //Arithmetic Operations with a integer,float and double numbers
-// Addition
+// Addition +
 console.log('Addition between integers:',25,'+',20, 'Result: ',25 + 20);
 console.log('Addition between floats:',25.5,'+',20.4, 'Result: ',25.5 + 20.4);
 console.log('Addition between doubles:',25.51,'+',20.41, 'Result: ',25.51 + 20.41);
 console.log('Addition between integer and float:',25,'+',20.1, 'Result: ',25 + 20.1);
 console.log('Addition between integer and double:',25,'+',20.123, 'Result: ',25 + 20.123);
 console.log('Addition between float and double:',25.1,'+',20.123, 'Result: ',25.1 + 20.123);
+// Subtraction -
+console.log('Subtraction between integers:',25,'-',20, 'Result: ',25 - 20);
+console.log('Subtraction between floats:',25.5,'-',20.4, 'Result: ',25.5 - 20.4);
+console.log('Subtraction between doubles:',25.51,'-',20.41, 'Result: ',25.51 - 20.41);
+console.log('Subtraction between integer and float:',25,'-',20.1, 'Result: ',25 - 20.1);
+console.log('Subtraction between integer and double:',25,'-',20.123, 'Result: ',25 - 20.123);
+console.log('Subtraction between float and double:',25.1,'-',20.123, 'Result: ',25.1 - 20.123);
+//Multiply *
 
+//Modulus /
+
+//Reminder %
