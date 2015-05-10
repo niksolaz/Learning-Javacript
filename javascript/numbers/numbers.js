2724186f5a453bfb855bc5be60679dfb120591fe
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-10 12:21:31 +0000

    Printing both float and double numbers

--- javascript/numbers/numbers.js
+++ javascript/numbers/numbers.js
@@ -4,3 +4,9 @@ console.log("Result", 1 + 1);
 // Integer number 
 var integerNumber = 20;
 console.log('This is an integer number',integerNumber);
+// Float number
+var floatNumber = 20.1;
+console.log('This is a float number',floatNumber);
+// Double number
+var doubleNumber = 20.1233;
+console.log('This is a double number',doubleNumber);
