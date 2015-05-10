7541c21092059c7501a5448ef3bce2d99831acde
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-10 12:50:57 +0000

    Arithmetic operation: modulus

--- javascript/numbers/numbers.js
+++ javascript/numbers/numbers.js
@@ -27,12 +27,17 @@ console.log('Subtraction between integer and float:',25,'-',20.1, 'Result: ',25 
 console.log('Subtraction between integer and double:',25,'-',20.123, 'Result: ',25 - 20.123);
 console.log('Subtraction between float and double:',25.1,'-',20.123, 'Result: ',25.1 - 20.123);
 //Multiply *
-console.log('Multiply between integers:',25,'*',20, 'Result: ',25 * 20);
-console.log('Multiply between floats:',25.5,'*',20.4, 'Result: ',25.5 * 20.4);
-console.log('Multiply between doubles:',25.51,'*',20.41, 'Result: ',25.51 * 20.41);
-console.log('Multiply between integer and float:',25,'*',20.1, 'Result: ',25 * 20.1);
-console.log('Multiply between integer and double:',25,'*',20.123, 'Result: ',25 * 20.123);
-console.log('Multiply between float and double:',25.1,'*',20.123, 'Result: ',25.1 * 20.123);
+console.log('Multiply between integers:',5,'*',2, 'Result: ',5 * 2);
+console.log('Multiply between floats:',5.5,'*',2.4, 'Result: ',5.5 * 2.4);
+console.log('Multiply between doubles:',5.51,'*',2.41, 'Result: ',5.51 * 2.41);
+console.log('Multiply between integer and float:',5,'*',2.1, 'Result: ',5 * 2.1);
+console.log('Multiply between integer and double:',5,'*',2.123, 'Result: ',5 * 2.123);
+console.log('Multiply between float and double:',5.1,'*',2.123, 'Result: ',5.1 * 2.123);
 //Modulus /
-
+console.log('Modulus between integers:',5,'/',2, 'Result: ',5 / 2);
+console.log('Modulus between floats:',5.5,'/',2.4, 'Result: ',5.5 / 2.4);
+console.log('Modulus between doubles:',5.51,'/',2.41, 'Result: ',5.51 / 2.41);
+console.log('Modulus between integer and float:',5,'/',2.1, 'Result: ',5 / 2.1);
+console.log('Modulus between integer and double:',5,'/',2.123, 'Result: ',5 / 2.123);
+console.log('Modulus between float and double:',5.1,'/',2.123, 'Result: ',5.1 / 2.123);
 //Reminder %
