094316827c326d8469f59ae83fd351c7172314da
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-11 19:13:21 +0000

    Learning about a String

---
+++ javascript/strings/strings.js
@@ -0,0 +1,12 @@
+// Learning about strings in javascript
+console.log('I am a String');
+console.log('This numeber is a string 12345');
+console.log("This returns the length of HelloWorld ('HelloWorld.length'):",'HelloWorld'.length);
+console.log("Helloworld".charAt(5));
+console.log('I am Jessi White'.replace('Jessi','Walter'));
+console.log('helloworld'.toUpperCase());
+console.log('HELLOWORLD'.toLowerCase());
+console.log('HELLO WORLD'.replace('WORLD','WALTER').toLowerCase());
+console.log('HELLO WORLD'.replace('WORLD','WALTER').toUpperCase());
+console.log('HELLO WORLD'.charAt(6).toLowerCase());
+console.log('Walter White'.charAt(8).toUpperCase());
