3bea3d4b7af09fa87f34e955ccc212c583cf73b7
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-12 19:33:28 +0000

    Printing variables methods

---
+++ javascript/variables/variables.js
@@ -0,0 +1,37 @@
+// Printing Variables
+var a = "string";
+console.log(a);
+
+var a = 1234;
+console.log(a);
+
+var a = "string"+ 1234;
+console.log(a);
+
+var a = 4 + 5;
+console.log(a);
+var a = 5 - 4;
+console.log(a);
+var a = 4 * 5;
+console.log(a);
+var a = 5 / 2;
+console.log(a);
+var a = 5 % 4;
+console.log(a);
+
+var a = "1234"+"1234";
+console.log(a);
+var a = "5"*"4";
+console.log(a);
+var a = "xx"*"yy";
+console.log(a);
+
+var a = 10;
+var b = 5;
+console.log(a>b,a<b,a==b,a!=b);
+console.log(a+b,a*b,a-b,a/b,a%b);
+
+var a = "I am";
+var b = " legend";
+var c = a + b;
+console.log(c);
