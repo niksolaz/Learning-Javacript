3bc525e2a8cb9d0efbb092436efb673b4d58895c
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-15 17:31:59 +0000

    printed new varaibles city, cap and address

--- javascript/variables/variables.js
+++ javascript/variables/variables.js
@@ -49,3 +49,11 @@ var person = {
 };
 console.log(person);
 console.log(person.name,person.city);
+
+var city = { nameCity: 'Asti'}
+var cap = {number: 14100};
+console.log(city.nameCity);
+console.log(cap.number);
+
+var address = city.nameCity +' '+ cap.number;
+console.log(address);
