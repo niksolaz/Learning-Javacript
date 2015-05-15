bf158313682ba935621c14f0b2d7ff755cbb2ba3
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-15 17:48:20 +0000

    printed logic operators

---
+++ javascript/boolean/boolean.js
@@ -0,0 +1,9 @@
+// Printing Booleans object 
+
+var x = false;
+var y = true;
+console.log(x&&y,x&&x,y&&y); // *
+console.log(x||y,x||x,y||y); // +
+
+console.log(x == y,x == x,y == y);
+console.log(x!=y, x!=x, y!=y);
