123282cf10c5fb8db519ec80a0324bf2fb1a3904
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-15 18:16:41 +0000

    printed all the Boolean logical operators

--- javascript/boolean/boolean.js
+++ javascript/boolean/boolean.js
@@ -4,6 +4,7 @@ var x = false;
 var y = true;
 console.log(x&&y,x&&x,y&&y); // AND
 console.log(x||y,x||x,y||y); // OR
+console.log(!x,!y); // NOT
 
 console.log(x == y,x == x,y == y); // EQUAL TO
 console.log(x!=y, x!=x, y!=y); // NOT EQUAL
@@ -25,3 +26,8 @@ console.log(a <= b); // less than or equal to
 console.log('The a == c is not equal, beacuse a is number and c is string. So result: ',a == c);
 
 console.log((a >= b) && (b <= a));
+
+console.log(Boolean(''));
+console.log(Boolean(234));
+
+
