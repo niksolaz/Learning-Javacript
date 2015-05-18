90c1106f477db696447f3226e80d2f0c63430490
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-18 19:03:43 +0000

    Getter methods and explanation syntax

---
+++ javascript/object/getter.js
@@ -0,0 +1,6 @@
+//getter methods
+
+console.log("Syntax: ");
+console.log("The get syntax binds an object property \n to a function that will be called when that property is looked up.");
+console.log("{get prop() { ... } }");
+console.log("{get [expression]() { ... } }");
