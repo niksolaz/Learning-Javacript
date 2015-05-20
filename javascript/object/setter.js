5d64c5c698adea2bc0554f06b61eb99debadac5e
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-20 19:49:45 +0000

    Logic syntax setter method

---
+++ javascript/object/setter.js
@@ -0,0 +1,6 @@
+//setter methods
+
+console.log("Syntax: ");
+console.log("The set syntax binds an object property to a function \n to be called when there is an attempt to set that property.");
+console.log("{set prop(val) { ... } }");
+console.log("{set [expression](val) { ... } }");
