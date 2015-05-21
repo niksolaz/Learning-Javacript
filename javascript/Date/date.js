882b845042d1b13e15a7da7ae80e604f67a0d687
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-21 15:53:26 +0000

    Example di Date Object for birthday

--- javascript/Date/date.js
+++ javascript/Date/date.js
@@ -15,9 +15,12 @@ new Date(year,month[,day[,hour[,minutes[,second[,milliseconds]]]]]);
 
 //Example: several ways to create a Date object
 var today = new Date();
-var birthday = new Date('May 21, 2015 17:44:00');
-var birthday = new Date();
-var birthday = new Date();
-var birthday = new Date();
+var birthdayOne = new Date('April 13, 1976 07:30:00');
+var birthdayTwo = new Date('1990-05-21T03:47:00');
+var birthdayThree = new Date(1982,08,25);
+var birthdayFour = new Date(1992,07,06,5,20,0);
 console.log(today);
-conosle.log(birthday);
+console.log(birthdayOne);
+console.log(birthdayTwo);
+console.log(birthdayThree);
+console.log(birthdayFour);
