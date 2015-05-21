451c8d77b67f46072d08f21a67967b6e7b2f2a83
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-21 15:45:50 +0000

    date object

---
+++ javascript/Date/date.js
@@ -0,0 +1,23 @@
+//Date
+
+console.log("==== Contructor ====");
+//Creates a JavaScript Date instance that represents a single moment in time. 
+//Date objects are based on a time value that is the number of milliseconds since 1 January, 1970 UTC.
+
+/*
+Contructor
+
+new Date();
+new Date(value);
+new Date(dateString);
+new Date(year,month[,day[,hour[,minutes[,second[,milliseconds]]]]]);
+*/
+
+//Example: several ways to create a Date object
+var today = new Date();
+var birthday = new Date('May 21, 2015 17:44:00');
+var birthday = new Date();
+var birthday = new Date();
+var birthday = new Date();
+console.log(today);
+conosle.log(birthday);
