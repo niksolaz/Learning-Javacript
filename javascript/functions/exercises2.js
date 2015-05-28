86a30ab2337c61d3f63bbd2827d7aea440f3e9f2
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-05-28 17:38:28 +0000

    New exercises of JavaScript: type argument and Fibonacci

--- javascript/functions/exercises2.js
+++ javascript/functions/exercises2.js
@@ -1,7 +1,12 @@
 /**
  * Write a JavaScript function which accepts an argument and returns the type.
  * */
-
+ console.log('----Exercises-------');
+ function typeOfArgument(arg){
+   return typeof arg;
+ }
+ console.log(typeOfArgument("casa"));
+ console.log('-----------------------');
 /**
  * Write a JavaScript program to compute the exponent of a number. 
    Note : The exponent of a number says how many times the base number is used as a factor.
@@ -14,8 +19,17 @@
   * Note : The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . 
   * Each subsequent number is the sum of the previous two.
   * */
-  
-  
+
+  var i;
+  var fib = [];
+  fib[0] = 0;
+  fib[1] = 1;
+  for(i=2; i<=10; i++)
+  {
+    fib[i] = fib[i-1] + fib[i-2];
+    console.log(fib[i]);
+  }
+  console.log('-----------------------');
   /**
    *  Write a JavaScript program to check whether a number is even or not.
    * */
@@ -65,3 +79,4 @@
    * Example string : 'Web Development Tutorial' 
    * Expected Output : 'Development'
    * */
+   
