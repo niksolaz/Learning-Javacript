f772da77c019755dfaca4e72954219b3932f71f7
niksolaz <solazzo.nicola@gmail.com>
2015-08-25 12:42:35 +0200

    tutorial5

--- javascript/reactjs/react-0.13.3/react-tutorial-master/public/tutorial5.js
+++ javascript/reactjs/react-0.13.3/react-tutorial-master/public/tutorial5.js
@@ -1,7 +1,9 @@
+
 var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
+        // two this.props.author
         <Comment author="Pietro Caccia">This is one comment</Comment>
         <Comment author="Giordano Marciatore">This is *another* comment</Comment>
       </div>
