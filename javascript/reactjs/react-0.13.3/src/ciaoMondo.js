086aaf7113a416742b9475b040de04044f64283f
niksolaz <solazzo.nicola@gmail.com>
2015-08-25 19:53:54 +0200

    prova elementi

---
+++ javascript/reactjs/react-0.13.3/src/ciaoMondo.js
@@ -0,0 +1,16 @@
+React.render(
+  React.createElement('h1', null, 'Ciao, Mondo!'),
+  document.getElementById('example')
+);
+
+React.render(
+  React.createElement('h2',null,'Ciao Gente'),
+  document.getElementById('example2')
+);
+
+React.render(
+  React.createElement('h3',null,'elemento h3'),
+  document.getElementById('commentList')
+);
+
+
