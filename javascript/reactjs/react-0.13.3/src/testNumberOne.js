63ce3f08550cf2a8da8efd64a7a4452631471b9f
niksolaz <solazzo.nicola@gmail.com>
2015-08-26 13:56:58 +0200

    checkbox

--- javascript/reactjs/react-0.13.3/src/testNumberOne.js
+++ javascript/reactjs/react-0.13.3/src/testNumberOne.js
@@ -78,3 +78,26 @@ var AppNav = (
 React.render(AppNav,document.getElementById('nav_container'));
 
 
+
+var FancyCheckbox = React.createClass({
+  render: function() {
+    var fancyClass = this.props.checked ? 'FancyChecked' : 'FancyUnchecked';
+    return (
+      <div className={fancyClass} onClick={this.props.onClick}>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+var FancyCheckbox = React.createElement(Div,null,{ 
+													className:fancyClass, 
+												   	onClick:this.props.onClick
+												   	);
+React.render(
+  <FancyCheckbox checked={true} onClick={console.log.bind(console)}>
+    Hello world!
+  </FancyCheckbox>,
+  document.getElementById('fancy_container')
+);
+
+
