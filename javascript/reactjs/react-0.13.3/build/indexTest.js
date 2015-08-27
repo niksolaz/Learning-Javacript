6cb480be7e79fe8677ef785de15c2caa16b38d16
niksolaz <solazzo.nicola@gmail.com>
2015-08-27 12:24:32 +0200

    multicomponent

---
+++ javascript/reactjs/react-0.13.3/build/indexTest.js
@@ -0,0 +1,5 @@
+//example Basic render 
+React.render( 
+        React.createElement('h1',null,'Render Basic of test Index'),
+        document.getElementById('example')
+      );
--- javascript/reactjs/react-0.13.3/src/indexTest.js
+++ javascript/reactjs/react-0.13.3/src/indexTest.js
@@ -3,3 +3,38 @@ React.render(
         React.createElement('h1',null,'Render Basic of test Index'),
         document.getElementById('example')
       );
+
+//Multicomponent example
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic username={this.props.username} />
+        <ProfileLink username={this.props.username} />
+      </div>
+    );
+  }
+});
+
+var ProfilePic = React.createClass({
+  render: function() {
+    return (
+      <img src={'https://graph.facebook.com/' + this.props.username + '/picture'} />
+    );
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <a href={'https://www.facebook.com/' + this.props.username}>
+        {this.props.username}
+      </a>
+    );
+  }
+});
+
+React.render(
+  <Avatar username="pwh" />,
+  document.getElementById('example_two')
+);
