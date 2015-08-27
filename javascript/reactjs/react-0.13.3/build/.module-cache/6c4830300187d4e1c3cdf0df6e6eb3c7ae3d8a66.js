b74cb9cda76ebaac1508bdaa81129eb172629533
niksolaz <solazzo.nicola@gmail.com>
2015-08-27 11:56:31 +0200

    new test index

---
+++ javascript/reactjs/react-0.13.3/build/.module-cache/6c4830300187d4e1c3cdf0df6e6eb3c7ae3d8a66.js
@@ -0,0 +1,4 @@
+React.render(
+        React.createElement("h1", null, "Test number Two..!"),
+        document.getElementById('example')
+      );
---
+++ javascript/reactjs/react-0.13.3/build/.module-cache/9dfb53ed249410963f9ba9c8d3489145358ce4fa.js
@@ -0,0 +1,4 @@
+React.render(
+        React.createElement('h1',null,'Test Number Two..!'),
+        document.getElementById('example')
+      );
