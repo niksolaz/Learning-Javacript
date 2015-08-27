ef27a232ff5eb0fbdb028138a9a6f11898e8198c
niksolaz <solazzo.nicola@gmail.com>
2015-08-27 15:22:01 +0200

    message in jsx

--- javascript/reactjs/react-0.13.3/src/indexTest.js
+++ javascript/reactjs/react-0.13.3/src/indexTest.js
@@ -4,7 +4,7 @@ React.render(
         document.getElementById('example')
       );
 //form //
-var msg = (
+var Msg = (
 	<Div style="color:#DC143C">
 		<Div.Form method="post">
 			<Div.Form.textarea rows="4" cols="50"/>
@@ -13,3 +13,18 @@ var msg = (
 		</Div.Form>
 	</Div>
 );
+var Msg = (
+	React.createElement(Div,null,{ style:"color:#DC143C" },
+		React.createElement(Div.Form,null,{ method:"post" },
+			React.createElement(Div.Form.textarea,null,{ 
+														rows:"4",
+														cols:"50" },
+				React.createElement(Div,null,{ 
+												type:"submit",
+												value:"Submit" },
+				)
+			)
+		)
+	)
+);
+React.render(Msg,document.getElementById('message'));
