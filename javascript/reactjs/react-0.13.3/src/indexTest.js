c3027bbcea56f52c1dbdd301bdd5070f91a4a7c1
niksolaz <solazzo.nicola@gmail.com>
2015-08-27 15:29:02 +0200

    Refactorin form

--- javascript/reactjs/react-0.13.3/src/indexTest.js
+++ javascript/reactjs/react-0.13.3/src/indexTest.js
@@ -8,7 +8,6 @@ var Msg = (
 	<Div style="color:#DC143C">
 		<Div.Form method="post">
 			<Div.Form.textarea rows="4" cols="50"/>
-			<Div.Form.label type="text"/>
 			<Div.Form.button type="submit" value="Submit"/>
 		</Div.Form>
 	</Div>
@@ -17,9 +16,9 @@ var Msg = (
 	React.createElement(Div,null,{ style:"color:#DC143C" },
 		React.createElement(Div.Form,null,{ method:"post" },
 			React.createElement(Div.Form.textarea,null,{ 
-														rows:"4",
-														cols:"50" },
-				React.createElement(Div,null,{ 
+														rows:"10",
+														cols:"100" },
+				React.createElement(Div.Form.button,null,{ 
 												type:"submit",
 												value:"Submit" },
 				)
