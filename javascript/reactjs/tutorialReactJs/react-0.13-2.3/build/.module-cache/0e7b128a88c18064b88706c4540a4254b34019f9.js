818c526124414abd2b9daa60aa6cafaf06c4b11e
niksolaz <solazzo.nicola@gmail.com>
2015-08-28 15:43:02 +0200

    comment in build/tutorial.js

--- javascript/reactjs/tutorialReactJs/react-0.13-2.3/build/.module-cache/0e7b128a88c18064b88706c4540a4254b34019f9.js
+++ javascript/reactjs/tutorialReactJs/react-0.13-2.3/build/.module-cache/0e7b128a88c18064b88706c4540a4254b34019f9.js
@@ -1,3 +1,9 @@
+/*
+This file is genereted after create the following src/"name_file".js
+First install in command-line tools: npm install -g react-tools
+Second moment, translate your src/"name_file".js in build/"name_file".js with:
+jsx --watch src/  build/
+*/
 React.render(
         React.createElement("h1", null, "Hello Tutorial React!"),
         document.getElementById('example')
