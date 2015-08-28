099a25383e005cfb13d5e3ce144eaf03ade99010
niksolaz <solazzo.nicola@gmail.com>
2015-08-28 18:14:03 +0200

    addeed events

--- javascript/reactjs/tutorialReactJs/react-0.13-2.3/src/tutorial.js
+++ javascript/reactjs/tutorialReactJs/react-0.13-2.3/src/tutorial.js
@@ -6,18 +6,28 @@ React.render(
       );
 //created first component MyFirstComponent
 var MyFirstComponent = React.createClass({ //created class with craeteClass
-	getInitialState: function(){ //specs
+	//events
+	incrementCount: function(){
+		this.setState({
+			count: this.state.count + 1
+		});
+	},
+	//specs
+	getInitialState: function(){ 
 		return {
-			count: 5
+			count: 0
 		}
 	},
 	render: function(){ //used method render ; it's return syntax DOM in html
 		return (
-			<h2>{this.props.count}</h2> //create props
+			<div class="my-component">
+        		<h1>Count: {this.state.count}</h1> //create props
+        		<button type="button" onClick={this.incrementCount}>Increment</button>
+      		</div> 
 		);
 	}
 });
 React.render(  
-	<MyFirstComponent name="SomeName"/>, //component created! added props name
+	<MyFirstComponent/>, //component created! 
 	document.getElementById('myDiv') //myDiv name attribute id
 );
