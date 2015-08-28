a2f8f613163e6ecc8023418619d0904872c2d9ba
niksolaz <solazzo.nicola@gmail.com>
2015-08-28 16:53:56 +0200

    addeed props

--- javascript/reactjs/tutorialReactJs/react-0.13-2.3/src/tutorial.js
+++ javascript/reactjs/tutorialReactJs/react-0.13-2.3/src/tutorial.js
@@ -8,11 +8,11 @@ React.render(
 var MyFirstComponent = React.createClass({ //created class with craeteClass
 	render: function(){ //used method render ; it's return syntax DOM in html
 		return (
-			<h2> Hello. I am my first Component</h2>
+			<h2> Hello {this.props.name}. I am my first Component</h2> //create props
 		);
 	}
 });
 React.render(  
-	<MyFirstComponent/>, //component created!
+	<MyFirstComponent name="SomeName"/>, //component created! added props name
 	document.getElementById('myDiv') //myDiv name attribute id
 );
