ae38d6c3d078724bbceb18f404136fcdd067d157
niksolaz <solazzo.nicola@gmail.com>
2015-08-29 16:43:15 +0200

    props

--- javascript/reactjs/tako-black/views/index.jsx
+++ javascript/reactjs/tako-black/views/index.jsx
@@ -17,7 +17,21 @@ var TodoList = React.createClass({
   render: function() {
     return (
       <div className = "todoList">
-        I am a TodoList.
+        <table style={{border: "2px solid black"}}>
+          <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
+var Todo = React.createClass({
+  render: function() {
+    return (
+      <div className = "todo">
+        <p>{this.props.title}</p>
       </div>
     );
   }
