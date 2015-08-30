66d1257219d29c396edf09d134707e06164570d2
niksolaz <solazzo.nicola@gmail.com>
2015-08-30 17:00:10 +0200

    added input

--- javascript/reactjs/tako-black/views/index.jsx
+++ javascript/reactjs/tako-black/views/index.jsx
@@ -29,11 +29,12 @@ var TodoList = React.createClass({
 });
 var Todo = React.createClass({
   propTypes: {
-    title: React.PropTypes.number.isRequired
+    title: React.PropTypes.string.isRequired
   },
   render: function() {
     return (
       <tr>
+        <td style={{border:"1px solid black"}}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/></td>
         <td style={{border:"1px solid black"}}>{this.props.title}</td>
         <td style={{border:"1px solid black"}}>{this.props.children}</td>
       </tr>
