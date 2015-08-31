2e72d86558c3bf7702ce26fec1bbbf688328f12d
niksolaz <solazzo.nicola@gmail.com>
2015-08-31 14:58:44 +0200

    use bundle

--- javascript/reactjs/tako-black/program.js
+++ javascript/reactjs/tako-black/program.js
@@ -20,8 +20,31 @@ var data = [
 			{title:'Hair cut',key:'Hair cut',detail:process.argv[4]}
 			];
 
-app.use('/', function(req, res) {
-  res.render('index', {data: data});
+app.use('/bundle.js', function(req, res) { 
+      res.setHeader('content-type', 'application/javascript'); 
+      browserify('./app.js') 
+        .transform('reactify') 
+        .bundle() 
+        .pipe(res); 
+    });
+    
+app.use('/', function(req, res) { 
+  var initialData = JSON.stringify(data); 
+  var markup = React.renderToString(React.createElement(TodoBox, {data: data}));
+
+  res.setHeader('Content-Type', 'text/html'); 
+
+  var html = React.renderToStaticMarkup(body(null, 
+      div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}), 
+      script({id: 'initial-data', 
+              type: 'text/plain', 
+              'data-json': initialData 
+            }), 
+      script({src: '/bundle.js'}) 
+  )); 
+
+  res.end(html); 
 });
+
 
 app.listen(app.get('port'), function() {});
