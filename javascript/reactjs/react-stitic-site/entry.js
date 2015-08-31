c8905976e913104d6e5c381e61a610cb44eacadb
niksolaz <solazzo.nicola@gmail.com>
2015-08-31 20:12:57 +0200

    create entry.js

--- javascript/reactjs/react-stitic-site/entry.js
+++ javascript/reactjs/react-stitic-site/entry.js
@@ -1,7 +1,8 @@
-var React = require('react')
-var Root = require('./components/Root.jsx')
+var React = require('react');
+var Router = require('react-router');
+var Routes = require('./Routes.jsx');
 
 module.exports = function render(locals, callback) {
-  var html = React.renderToStaticMarkup(React.createElement(Root, locals))
+  var html = React.renderToStaticMarkup(React.createElement(Handler, locals))
   callback(null, '<!DOCTYPE html>' + html)
-}
+};
